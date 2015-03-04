606b239a20ef3ac1f27c1101e5e149adb89449ce
Giampli <france.marco@gmail.com>
2015-03-05 00:21:20 +0100

    Start
    Le cose

---
+++ Assets/Scripts/ClassicControl.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassicControl : MonoBehaviour {
+	Vector2 velocity;
+	Vector2 target;
+
+	void Start (){
+		velocity = new Vector2 (0, 0);
+	}
+
+	public void setLeft(){
+		velocity.x=-1;
+	}
+	
+	public void setRight(){
+		velocity.x=1;
+	}
+	
+	public void setUp(){
+		velocity.y=1;
+	}
+	
+	public void setDown(){
+		velocity.y=-1;
+	}
+
+	public void setYtoZero(){
+		velocity.y = 0;
+	}
+
+	public void setXtoZero(){
+		velocity.x = 0;
+	}
+
+	public Vector2 GetVelocity(){
+		return velocity;
+	}
+
+	public void Robba(){
+		print ("Robba");
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public ClassicControl control;
+	Vector2 velocity;
+	public float speed;
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent <Animator>();
+	}
+	
+	void Update () {
+		anim.SetFloat ("XVel", control.GetVelocity ().x);
+		anim.SetFloat ("YVel", control.GetVelocity ().y);
+		rigidbody2D.velocity = control.GetVelocity () * speed * Time.deltaTime;
+	}
+}
